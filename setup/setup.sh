cdc5bf2ea175e5eaa85da6e3aeda97dd6d88cc4b
Simon Lammer <lammer.simon@gmail.com>
2018-01-12 11:06:38 +0100

    Provide script to run gradle from a blank system

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+echo "     _                                            _____      _               
+    | |                                          /  ___|    | |              
+ ___| | __ _ _ __ ___  _ __ ___   ___ _ __ ______\\ \`--.  ___| |_ _   _ _ __  
+/ __| |/ _\` | '_ \` _ \\| '_ \` _ \\ / _ \\ '__|______|\`--. \\/ _ \\ __| | | | '_ \\ 
+\\__ \\ | (_| | | | | | | | | | | |  __/ |         /\\__/ /  __/ |_| |_| | |_) |
+|___/_|\\__,_|_| |_| |_|_| |_| |_|\\___|_|         \\____/ \\___|\\__|\\__,_| .__/ 
+                                                                      | |    
+                                                                      |_|    "
+
+# http://patorjk.com/software/taag/#p=display&f=Doom&t=slammer-Setup
+
+# Constants
+workdir=/tmp/slammer-setup
+
+# move to temporary directory
+mkdir $workdir
+cd $workdir
+
+# download repo to $workdir/repo
+wget -O repo.zip https://github.com/SimonLammer/dotfiles/archive/setup.zip
+mkdir repo_
+unzip repo.zip -d repo_
+name=$(ls repo_)
+mv repo_/$name repo
+rmdir repo_
+
+# install java (so gradle works)
+apt-get install -y openjdk-8-jre
+
+# check gradle version
+repo/setup/gradlew --version
+
+# remove temporary directory
+rm -R $workdir
