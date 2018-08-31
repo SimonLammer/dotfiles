0e173ba526ac3a1587055a43c04ea74c9770a047
Simon Lammer <lammer.simon@gmail.com>
2018-08-31 17:09:31 +0200

    Grgit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
-tasks.create("foo") {
-  System.out.println("Hello World!")
+import org.ajoberstar.grgit.Grgit
+
+plugins {
+  id("org.ajoberstar.grgit") version "3.0.0-beta.1" // http://ajoberstar.org/grgit/grgit-reference.html
+}
+
+repositories {
+  jcenter()
+}
+
+tasks {
+  val foo by creating {
+    println("Hello World!")
+  }
+  
+  val gitls by creating {
+    val repo = Grgit.open()
+    println(repo.head().author.name)
+  }
 }
