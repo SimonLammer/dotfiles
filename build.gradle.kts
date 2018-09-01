e20143e5a0035ea4725f64e933953effa6813146
Simon Lammer <lammer.simon@gmail.com>
2018-09-01 18:05:19 +0200

    Mustache with function

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
-import com.github.mustachejava.*
 import java.io.*
+import com.github.mustachejava.*
+import org.yaml.snakeyaml.*
 
 plugins {}
 repositories {}
@@ -9,14 +10,31 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("com.github.spullara.mustache.java:compiler:0.9.5")
+    classpath("com.github.spullara.mustache.java:compiler:+")
+    classpath("org.yaml:snakeyaml:+")
   }
 }
 
 tasks {
   val mustache by creating {
-    val mf = DefaultMustacheFactory()
-    val mustache = mf.compile(StringReader("Hello {{text}}"), "greeting")
-    mustache.execute(PrintWriter(System.out), hashMapOf("text" to "world")).flush()
+    doLast {
+      val yaml = Yaml().load(FileInputStream(File("mustache-data.yml")))
+      val map = mapOf(
+        "foo" to "bar",
+        "num" to 4,
+        "first" to java.util.function.Function<String, String> { it.substring(0, it.indexOf("\n") + 1) },
+        "last" to java.util.function.Function<String, String> { it.substring(it.indexOf("\n")) }
+      )
+      val data = listOf(yaml, map)
+      val mf = DefaultMustacheFactory()
+      val mustache = mf.compile("template.mustache")
+      mustache.execute(PrintWriter(System.out), data).flush()
+    }
+  }
+
+  create("foo") {
+    doLast {
+      println("bar")
+    }
   }
 }
