973568a2c906563aa192ffd6116da7e518f95612
Simon Lammer <lammer.simon@gmail.com>
2018-09-04 07:35:01 +0200

    Fix mustache wrappers

--- gradle/external/mustache-wrappers.gradle.kts
+++ gradle/external/mustache-wrappers.gradle.kts
@@ -1,24 +1,43 @@
 extra["firstLine"] = fun(s: String): String {
   return processTrimmed(s) {
-    s.substring(0, s.indexOf("\n") + 1)
+    unlessSingleLine(it) {
+      it.substring(0, it.indexOf("\n"))
+    }
   }
 }
 
 extra["lastLine"] = fun(s: String): String {
-  return processTrimmed(s) {
-    s.substring(s.lastIndexOf("\n"))
+  return processTrimmedEnd(s) {
+    it.substring(it.lastIndexOf("\n") + 1)
   }
 }
 
 extra["withoutLastLine"] = fun(s: String): String {
   return processTrimmed(s) {
-    it.substring(0, it.lastIndexOf("\n")) // TODO implement & test
+    it.substring(0, Math.max(0, it.lastIndexOf("\n")))
   }
 }
 
 fun processTrimmed(s: String, f: (s: String) -> String): String {
+  return processTrimmedStart(s) {
+    processTrimmedEnd(it, f)
+  }
+}
+
+fun processTrimmedStart(s: String, f: (s: String) -> String): String {
   val startIndex = s.length - s.trimStart().length
-  val endIndex   = s.trimEnd().length
-  //println("pT: s.length = ${s.length}; startIndex = $startIndex; endIndex = $endIndex; s = '${s.substring(startIndex, endIndex)}' -> '${f(s.substring(startIndex, endIndex))}'")
-  return "${s.substring(0, startIndex)}${f(s.substring(startIndex, endIndex))}${s.substring(endIndex)}"
+  return "${s.substring(0, startIndex)}${f(s.substring(startIndex))}"
+}
+
+fun processTrimmedEnd(s: String, f: (s: String) -> String): String {
+  val endIndex = s.trimEnd().length
+  return "${f(s.substring(0, endIndex))}${s.substring(endIndex)}"
+}
+
+fun unlessSingleLine(s: String, f: (s: String) -> String): String {
+  return if (s.indexOf("\n") == -1) {
+    s
+  } else {
+    f(s)
+  }
 }
