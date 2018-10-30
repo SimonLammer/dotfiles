a7f6522b0628e4e80a9191c32a3805bdde49502f
Simon Lammer <lammer.simon@gmail.com>
2018-10-30 17:00:29 +0100

    Gradle: add file dependency & print rendered files

--- gradle/tasks/template/mustache.gradle.kts
+++ gradle/tasks/template/mustache.gradle.kts
@@ -79,7 +79,7 @@ val testTask = tasks.create("renderTemplatesTest") {
 }
 tasks.named("test").configure { dependsOn(testTask) }
 val renderTask = tasks.create("renderTemplates") {
-  dependsOn(createTemplateRenderingTasks(File("data")))
+  dependsOn(createTemplateRenderingTasks(File("data"), verbose=true))
   shouldRunAfter("test")
   doLast {
     println(Cowsay.say(arrayOf("-f", "default", "I've successfully rendered all of your mustache templates.")))
@@ -127,41 +127,46 @@ fun assertFileContentIsEqual(a: File, e: File) {
   }
 }
 
-fun createTemplateRenderingTasks(dir: File): Iterable<Task> {
+fun createTemplateRenderingTasks(dir: File, verbose: Boolean = false): Iterable<Task> {
   require(dir.isDirectory())
   return dir
     .walkTopDown()
     .filter { isTemplateFile(it) }
-    .map { createTemplateRenderingTask(it) }
+    .map { createTemplateRenderingTask(it, verbose) }
     .toList()
 }
 
 fun isTemplateFile(file: File) = file.isFile() && file.name.endsWith(templateFileExtension)
 
-fun createTemplateRenderingTask(file: File): Task {
+fun createTemplateRenderingTask(file: File, verbose: Boolean = false): Task {
   val (dataFiles, templateDataFileRenderingTaskNames) = fetchTemplateDataFiles(file)
   val output = File(file.parentFile, file.name.substring(0, file.name.length - templateFileExtension.length))
   logger.info("Creating template rendering task for $file")
   return tasks.create(templateRenderingTaskName(file)) {
+    inputs.files(listOf(file))
     inputs.files(helperFiles)
     inputs.files(dataFiles)
     outputs.file(output)
     shouldRunAfter("test")
 
     if (templateDataFileRenderingTaskNames.size > 0) {
-      logger.info("DependsOn:")
+      logger.info("\tDependsOn:")
     }
     templateDataFileRenderingTaskNames.forEach {
-      logger.info("\t$it")
+      logger.info("\t\t$it")
       dependsOn(it)
       mustRunAfter(it)
     }
     
-    logger.info("DataFiles:")
-    dataFiles.forEach { logger.info("\t$it") }
+    logger.info("\tDataFiles:")
+    dataFiles.forEach { logger.info("\t\t$it") }
 
     doLast {
-      logger.info("Rendering $file to $output")
+      if (verbose) {
+        logger.lifecycle("Rendering $file to $output")
+      } else {
+        logger.info("Rendering $file to $output")
+      }
       val data = mutableListOf<Any>(mustacheWrappers)
       data.addAll(
         transformData (
@@ -172,7 +177,7 @@ fun createTemplateRenderingTask(file: File): Task {
           )
         )
       )
-      logger.info("Data: $data")
+      logger.debug("Data: $data")
       val mustache = mustacheFactory.compile(InputStreamReader(FileInputStream(file), Charsets.UTF_8), file.absolutePath)
       mustache.execute(FileWriter(output), data).flush()
     }
@@ -220,247 +225,3 @@ fun concatTemplateDataFileInputStreams(streams: Iterable<InputStream>) :InputStr
     SequenceInputStream(Collections.enumeration(concatinatedInputStreams))
   }
 }
-
-// ---------------------------------------------------------------------------------------------
-
-open class TemplateRenderer : DefaultTask() {
-  lateinit var file: File
-
-  @TaskAction
-  fun renderTemplates() {
-    require(file.isFile()) { "TemplateRenderer.file must be a file!\n\tfile: $file"}
-    println("Hello $file")
-  }
-}
-
-val a = tasks.create<TemplateRenderer>("debug") {
-  file = File("Readme.md")
-  dependsOn(listOf())
-}
-
-fun r(n: Int): Task {
-  return tasks.create("r$n") {
-    if (n > 1) {
-      dependsOn(r(n-1))
-    }
-    doLast {
-      println("$n")
-    }
-  }
-}
-r(3)
-
-//apply {
-  //  listOf(MUSTACHE_WRAPPERS_GRADLE, DATA_TRANSFORMERS_GRADLE).forEach {
-    //    from(it.path)
-  //  }
-//}
-
-/*
-val MUSTACHE_DATA_FILENAME          = extra["mustacheDataFilename"]  as String
-val MUSTACHE_DATA_FILE_CONCATINATOR = extra["mustacheDataFileConcatinator"] as String
-val MUSTACHE_EXT                    = extra["mustacheExt"]           as String
-val MUSTACHE_PARTIAL_EXT            = extra["mustachePartialExt"]    as String
-val MUSTACHE_PARTIAL_PREFIX         = extra["mustachePartialPrefix"] as String
-val MUSTACHE_PARTIAL_SUFFIX         = extra["mustachePartialSuffix"] as String
-
-val transformData = extra["transform-data"] as (Iterable<Any>) -> Iterable<Any>
-
-val GRADLE_PROPERTIES = File("gradle.properties")
-
-val YAML = Yaml()
-val MUSTACHE = "mustache"
-val MUSTACHE_FACTORY = DefaultMustacheFactory()
-MUSTACHE_FACTORY.objectHandler = object : ReflectionObjectHandler() {
-  override fun find(name: String, scopes: List<Any?>): com.github.mustachejava.util.Wrapper {
-    val wrapper = super.find(name, scopes)
-    if (wrapper is MissingWrapper) {
-      throw RuntimeException("Mustache variable '$name' not in scope!")
-    }
-    return wrapper
-  }
-}
-val MUSTACHE_WRAPPERS = listOf( // will be mapped to mapOf<String, Any>
-  "debug",
-  "firstLine",
-  "lastLine",
-  "withoutLastLine"
-).map {
-  val f = extra[it] as (s: String) -> String
-  it to Function<String, String> {
-    f(it)
-  }
-}.toMap<String, Any>()
-
-tasks {
-  val dotfilesGroup = "Mustache"
-
-  val mustacheTest = tasks.create("mustacheTest") {
-    group = dotfilesGroup
-
-    listOf( // clean before tests
-      File("gradle/external/tests/yaml-templates/after/mustache.yml"),
-      File("gradle/external/tests/yaml-templates/before/mustache.yml")
-    ).forEach {
-      if (it.exists()) {
-        it.delete()
-      }
-    }
-
-    doLast {
-
-    }
-  }
-
-  tasks.create("mustache") {
-    group = dotfilesGroup
-    dependsOn(mustacheTest)
-    
-    doLast {
-      println(Cowsay.say(arrayOf("-f", "stegosaurus", "I've successfully rendered all of your mustache templates.")))
-    }
-  }
-}
-
-File(".").walkTopDown().forEach { input ->
-  if (input.isFile() && input.name.endsWith(".$MUSTACHE_EXT") && !input.name.endsWith(".$MUSTACHE_PARTIAL_EXT")) {
-    val output = File(input.parent, "${input.name.substring(0, input.name.length - MUSTACHE_EXT.length - 1)}")
-    val taskName = mustacheTaskName(input)
-    logger.info("Creating mustache task for ${input.path} (\"$taskName\")")
-
-    val task = tasks.create(taskName) {
-      group = "$MUSTACHE"
-      val inputsFiles = mutableListOf(GRADLE_PROPERTIES, MUSTACHE_WRAPPERS_GRADLE, DATA_TRANSFORMERS_GRADLE, input)
-      val mustacheDataFiles = fetchMustacheDataFiles(input)
-      inputsFiles.addAll(mustacheDataFiles)
-      inputs.files(inputsFiles)
-      outputs.file(output)
-
-      // depend on mustache data files generated by mustache
-      mustacheDataFiles.forEach { file ->
-        val f = File(file.parentFile, "${file.name}.$MUSTACHE_EXT")
-        if (!input.equals(f) && f.exists()) {
-          dependsOn(mustacheTaskName(f))
-        }
-      }
-
-      if (!isTestMustacheFile(input)) {
-        mustRunAfter(tasks.named("${MUSTACHE}Test"))
-      }
-
-      doLast {
-        logger.lifecycle("Preparing for parsing of ${input.path} to ${output.path}")
-        val dataInputStreams = mustacheDataFiles.map {
-          logger.info("Adding data source ${it.path}")
-          FileInputStream(it)
-        }
-        logger.lifecycle("Loading data")
-        val data = mutableListOf<Any>(MUSTACHE_WRAPPERS)
-        data.addAll(
-          transformData (
-            YAML.loadAll(
-              concatMustacheInputStreams(
-                dataInputStreams
-              )
-            )
-          )
-        )
-        logger.info("Data: $data")
-        logger.lifecycle("Parsing")
-        val mustache = MUSTACHE_FACTORY.compile(StringReader("$MUSTACHE_PARTIAL_PREFIX${input.path}$MUSTACHE_PARTIAL_SUFFIX"), input.name)
-        mustache.execute(FileWriter(output), data).flush()
-      }
-    }
-
-    tasks.named("mustache").configure {
-      dependsOn(task)
-    }
-
-    if (isTestMustacheFile(input)) {
-      val testTask = tasks.create("mustacheTest${taskName.substring(taskName.indexOf('#'))}") {
-        group = "${MUSTACHE}Test"
-        dependsOn(task)
-        val expectationFile = File(input.parent, "${input.name.substring(0, input.name.length - MUSTACHE_EXT.length - 1)}.expected")
-        val inputsFiles = mutableListOf(expectationFile)
-        inputsFiles.addAll(task.inputs.files.files)
-        inputs.files(inputsFiles)
-        outputs.files(listOf())
-
-        doLast {
-          logger.lifecycle("Asserting that '${output.path}' and '${expectationFile.path}' are equal in content")
-          val expected = BufferedReader(FileReader(expectationFile))
-          val actual   = BufferedReader(FileReader(output))
-
-          fun throwGradleException(s: String) {
-            expected.close()
-            actual.close()
-            throw GradleException(s)
-          }
-
-          var lineNumber = 0
-          while (true) {
-            lineNumber++
-            val expectedLine = expected.readLine()
-            val actualLine   = actual.readLine()
-
-            if ((expectedLine == null) != (actualLine == null)) {
-              throwGradleException("Number of lines doesn't match!")
-            }
-            if (expectedLine == null || actualLine == null) {
-              break
-            }
-            if (!actualLine.equals(expectedLine)) {
-              throwGradleException("Line ${lineNumber} doesn't match!\n\texpected: '$expectedLine'\n\t     got: '$actualLine'")
-            }
-          }
-        }
-      }
-      tasks.named("mustacheTest").configure {
-        dependsOn(testTask)
-      }
-    }
-  }
-}
-
-fun mustacheTaskName(file: File): String = "$MUSTACHE#${file.path.substring(2).replace("/", ",")}"
-
-fun isTestMustacheFile(file: File): Boolean = file.path.startsWith("./gradle/external/tests")
-
-fun fetchMustacheDataFiles(file: File): List<File> {
-  var dir: File? = file.parentFile
-  var files = mutableListOf<File>()
-  while (dir != null) {
-    val dataFile = File(dir, MUSTACHE_DATA_FILENAME)
-    val dataFileTemplate = File(dir, "$MUSTACHE_DATA_FILENAME.$MUSTACHE_EXT")
-    if (!file.equals(dataFileTemplate) && (dataFileTemplate.exists() || dataFile.exists())) {
-      files.add(dataFile)
-    }
-
-    dir = dir?.parentFile
-  }
-  Collections.reverse(files)
-  return files
-}
-
-fun concatInputStreams(streams: Iterable<InputStream>, concatinatorFactory: () -> InputStream) :InputStream {
-  val iterator = streams.iterator()
-  return if (!iterator.hasNext()) {
-    ByteArrayInputStream(byteArrayOf())
-  } else {
-    var concatinatedInputStreams = mutableListOf(iterator.next())
-    while (iterator.hasNext()) {
-      concatinatedInputStreams.add(concatinatorFactory())
-      concatinatedInputStreams.add(iterator.next())
-    }
-    SequenceInputStream(Collections.enumeration(concatinatedInputStreams))
-  }
-}
-
-fun mustacheFileConcatinatorInputStream(): InputStream {
-  val concatinatorByteArray = MUSTACHE_DATA_FILE_CONCATINATOR.toByteArray(Charsets.UTF_8)
-  return ByteArrayInputStream(concatinatorByteArray)
-}
-
-fun concatMustacheInputStreams(streams: Iterable<InputStream>) =
-  concatInputStreams(streams, { mustacheFileConcatinatorInputStream() })
-*/
