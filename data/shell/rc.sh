1eec2855f507460806334bbd5b2a9c439160579e
Simon Lammer <lammer.simon@gmail.com>
2020-06-09 19:25:57 +0200

    Tmux: migrate config to version 3

--- data/shell/rc.sh
+++ data/shell/rc.sh
@@ -1,11 +1,14 @@
 # This will be sourced from `~/.profile` as well as shells rc files (e.g. `~/.bashrc`)
 
-export DOTFILES_HOME="~/.config/dotfiles"
+export DOTFILES_HOME="$HOME/.config/dotfiles"
 
 alias gds='git diff --no-index --color-words="[^[:space:]]|([[:alnum:]]|UTF_8_GUARD)+" --word-diff=plain'
-alias gdiff='git diff --no-index'
-alias tmux='tmux -f "$XDG_CONFIG_HOME/tmux/tmux.conf"'
+alias la='ls -A'
+alias ll='ls -alF'
+alias l='ls -CF'
+alias tmux='tmux -f "${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf"'
 alias gits='git status'
+alias gdiff='git diff --no-index'
 
 if [ -f "$DOTFILES_HOME/data/shell/rc-local.sh" ]; then
     . "$DOTFILES_HOME/data/shell/rc-local.sh"
