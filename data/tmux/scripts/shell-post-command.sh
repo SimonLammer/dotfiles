1940a4462e6ed4b45e75f804a2bf359ee607bec6
Simon Lammer <lammer.simon@gmail.com>
2020-06-12 12:38:01 +0200

    TMUX: describe directory in post-command shell hook

---
+++ data/tmux/scripts/shell-post-command.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# This should be executed after every command
+# via the shell's post command hook.
+
+which tmux >/dev/null 2>&1
+if [ \
+    $? -eq 0 -a \
+    -n "$TMUX" -a \
+    -n "$TMUX_PANE" \
+]; then
+    PANE_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/tmux/panes/${TMUX_PANE#*%}"
+    mkdir -p "$PANE_CACHE_DIR"
+
+    python3 $DOTFILES_HOME/data/tmux/scripts/describe-directory/describe_directory.py "`pwd`" > "$PANE_CACHE_DIR/directory_description.txt"
+
+    # TODO: refresh tmux ?
+
+    tmux refresh-client
+
+    unset PANE_CACHE_DIR
+fi
