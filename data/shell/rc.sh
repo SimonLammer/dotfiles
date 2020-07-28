b7e3474c2f944660bc2b5a43568807b88afad68a
Simon Lammer <lammer.simon@gmail.com>
2020-07-28 13:03:50 +0200

    Shell: template rc.sh path additions

--- data/shell/rc.sh
+++ data/shell/rc.sh
@@ -2,13 +2,17 @@
 
 export DOTFILES_HOME="$HOME/.config/dotfiles"
 
-alias gds='git diff --no-index --color-words="[^[:space:]]|([[:alnum:]]|UTF_8_GUARD)+" --word-diff=plain'
-alias la='ls -A'
-alias ll='ls -alF'
-alias l='ls -CF'
-alias tmux='tmux -f "${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf"'
 alias gits='git status'
 alias gdiff='git diff --no-index'
+alias gds='git diff --no-index --color-words="[^[:space:]]|([[:alnum:]]|UTF_8_GUARD)+" --word-diff=plain'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias tmux='tmux -f "${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf"'
+
+if [ -d "/opt/anki/bin" ] ; then
+    PATH="/opt/anki/bin:$PATH"
+fi
 
 if [ -f "$DOTFILES_HOME/data/shell/rc-local.sh" ]; then
     . "$DOTFILES_HOME/data/shell/rc-local.sh"
