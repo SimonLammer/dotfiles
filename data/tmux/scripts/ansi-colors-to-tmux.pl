797ed1814eb7fde6d480610c70811362b54f9ed4
Simon Lammer <lammer.simon@gmail.com>
2020-12-23 14:29:28 +0100

    Tmux: ansi to tmux color - handle ESC[m

--- data/tmux/scripts/ansi-colors-to-tmux.pl
+++ data/tmux/scripts/ansi-colors-to-tmux.pl
@@ -15,44 +15,48 @@ use warnings;
 use strict;
 
 my @colors = ("black", "red", "green", "yellow", "blue", "magenta", "cyan", "white");
-while(/(.*?)(\x1b\[((\d+;?)+)m)/gc) {
+while(/(.*?)(\x1b\[((\d+;?)*)m)/gc) {
 	print "$1#[";
-	my @sgr = split /;/, $3;
-	for(my $i = 0; $i <= $#sgr; $i++) {
-		if ($sgr[$i] eq "0") {
-			print "default";
-		} elsif ($sgr[$i] eq "1") {
-			print "bright";
-		} elsif ($sgr[$i] =~ /(3|4|9|10)(\d)/) {
-			if ($1 eq "3") {
-				print "fg=";
-			} elsif ($1 eq "4") {
-				print "bg=";
-			} elsif ($1 eq "9") {
-				print "fg=bright";
-			} elsif ($1 eq "4") {
-				print "bg=bright";
-			}
-			if ($2 eq "8") { # SGR 38 or 48
-				$i++;
-				if ($sgr[$i] eq "5") {
-					$i++;
-					print "colour" . $sgr[$i];
-				} elsif ($sgr[$i] eq "2") {
-					printf("#%02X%02X%02X", $sgr[$i + 1], $sgr[$i + 2], $sgr[$i + 3]);
-					$i += 3;
-				} else {
-					die "Invalid SGR 38;" . $sgr[$i];
-				}
-			} elsif ($2 eq "9") {
+	if ($3 eq "") {
+		print "default";
+	} else {
+		my @sgr = split /;/, $3;
+		for(my $i = 0; $i <= $#sgr; $i++) {
+			if ($sgr[$i] eq "0") {
 				print "default";
-			} else {
-				print $colors[$2];
+			} elsif ($sgr[$i] eq "1") {
+				print "bright";
+			} elsif ($sgr[$i] =~ /(3|4|9|10)(\d)/) {
+				if ($1 eq "3") {
+					print "fg=";
+				} elsif ($1 eq "4") {
+					print "bg=";
+				} elsif ($1 eq "9") {
+					print "fg=bright";
+				} elsif ($1 eq "4") {
+					print "bg=bright";
+				}
+				if ($2 eq "8") { # SGR 38 or 48
+					$i++;
+					if ($sgr[$i] eq "5") {
+						$i++;
+						print "colour" . $sgr[$i];
+					} elsif ($sgr[$i] eq "2") {
+						printf("#%02X%02X%02X", $sgr[$i + 1], $sgr[$i + 2], $sgr[$i + 3]);
+						$i += 3;
+					} else {
+						die "Invalid SGR 38;" . $sgr[$i];
+					}
+				} elsif ($2 eq "9") {
+					print "default";
+				} else {
+					print $colors[$2];
+				}
+			} else { # Unknown/ignored SGR code
+				next;
 			}
-		} else { # Unknown/ignored SGR code
-			next;
+			print ",";
 		}
-		print ",";
 	}
 	print "]";
 }
