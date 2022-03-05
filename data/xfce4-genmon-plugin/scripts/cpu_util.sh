71fe5deb09ef09802e774b52ae31707ea6760e3d
Simon Lammer <lammer.simon@gmail.com>
2022-03-05 14:28:11 +0100

    xfce4-genmon-plugin: logscale utilization progressbars

--- data/xfce4-genmon-plugin/scripts/cpu_util.sh
+++ data/xfce4-genmon-plugin/scripts/cpu_util.sh
@@ -3,12 +3,18 @@
 coloredbar="$DOTFILES_HOME/data/xfce4-genmon-plugin/scripts/coloredbar.sh"
 
 util=`awk -f $DOTFILES_HOME/data/scripts/cpu_utilization.awk`
+threads=`grep "processor" /proc/cpuinfo | wc -l`
+
+utillog=`awk "BEGIN{printf(\"%.0f\n\", 100*log($util + 1)/log(101))}"`
+low=`awk "BEGIN{printf(\"%.0f\n\", 100/$threads)}"`
 
 ################################################################################
 
-$coloredbar $util \
+# 25% util = 70% utillog
+# 87% util ~ 97% utillog
+$coloredbar $utillog \
   0 100 \
-  0 12 100 \
+  $low 70 97 \
   191 191 191 \
   240 240 0 \
   255 15 15
--- data/xfce4-genmon-plugin/scripts/gpu_util.sh
+++ data/xfce4-genmon-plugin/scripts/gpu_util.sh
@@ -5,11 +5,16 @@ coloredbar="$DOTFILES_HOME/data/xfce4-genmon-plugin/scripts/coloredbar.sh"
 # https://unix.stackexchange.com/a/358990/367736
 util=`nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits`
 
+utillog=`awk "BEGIN{printf(\"%.0f\n\", 100*log($util + 1)/log(101))}"`
+
 ################################################################################
 
-$coloredbar $util \
+#  5% util ~ 39% utillog
+# 25% util ~ 70% utillog
+# 87% util ~ 97% utillog
+$coloredbar $utillog \
   0 100 \
-  0 30 100 \
+  39 70 97 \
   191 191 191 \
   240 240 0 \
   255 15 15
