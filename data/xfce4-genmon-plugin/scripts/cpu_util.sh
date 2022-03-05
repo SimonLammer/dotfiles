306fb3673e536fd2927a233dc13db78f381a3817
Simon Lammer <lammer.simon@gmail.com>
2022-03-05 14:42:38 +0100

    xfce4-genmon-plugin: logscale utilization progressbars (revised)

--- data/xfce4-genmon-plugin/scripts/cpu_util.sh
+++ data/xfce4-genmon-plugin/scripts/cpu_util.sh
@@ -6,11 +6,11 @@ util=`awk -f $DOTFILES_HOME/data/scripts/cpu_utilization.awk`
 threads=`grep "processor" /proc/cpuinfo | wc -l`
 
 utillog=`awk "BEGIN{printf(\"%.0f\n\", 100*log($util + 1)/log(101))}"`
-low=`awk "BEGIN{printf(\"%.0f\n\", 100/$threads)}"`
+low=`awk "BEGIN{printf(\"%.0f\n\", 100*log(100/$threads + 1)/log(101))}"`
 
 ################################################################################
 
-# 25% util = 70% utillog
+# 25% util ~ 70% utillog
 # 87% util ~ 97% utillog
 $coloredbar $utillog \
   0 100 \
