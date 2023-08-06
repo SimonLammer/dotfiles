510d8493998353c134c589e81169065c614a6df3
Simon Lammer <lammer.simon@gmail.com>
2023-08-06 15:00:20 +0200

    Revise template generation

--- data/scripts/render_templates.py
+++ data/scripts/render_templates.py
@@ -1,11 +1,14 @@
 #!/bin/python3
 
+import difflib
 import logging
 import os
+import shutil
 import yaml
 from jinja2 import Environment, FileSystemLoader
 from jinja2_ansible_filters import AnsibleCoreFiltersExtension
 from glob import glob
+from tempfile import TemporaryDirectory
 
 from pdb import set_trace as st
 
@@ -15,15 +18,17 @@ TEMPLATE_EXTENSION = '.j2'
 
 
 LOGGER = logging.getLogger(__name__)
-logging.basicConfig(format="[%(asctime)s %(levelname)8s] %(message)s   (%(funcName)s@%(filename)s:%(lineno)s)")
+logging.basicConfig(format="[%(asctime)s %(levelname)8s] %(message)s   (L%(lineno)s)")
 LOGGER.setLevel(level=os.getenv('LOGLEVEL', LOGLEVEL_DEFAULT).upper())
 
 # TODO:
 # - Cache file hashsums / modification timestamps & don't process unless changed
 # - Backup old template output before rendering
 
-def main(directory: str) -> None:
-  LOGGER.info(f"Starting template renderer for directory {directory}")
+CONFIG = None
+
+def main(directory) -> None:
+  LOGGER.info(f"Starting template renderer for directory '{os.path.abspath(directory)}'")
   environment = create_template_environment(directory)
   variables = {}
 
@@ -31,7 +36,7 @@ def main(directory: str) -> None:
     ('vars', '*.yml'),
     ('data', 'vars.yml'),
   ):
-    LOGGER.info(f"Loading simple variables in '{folder}'")
+    LOGGER.info(f"Loading simple variable files in '{folder}'.")
     files = [
       f
       for pattern in (
@@ -45,6 +50,8 @@ def main(directory: str) -> None:
         variables,
         load_variables(files),
       )
+    else:
+      LOGGER.info(f"No simple variable files found in '{folder}'.")
 
     files = [
       f
@@ -55,13 +62,15 @@ def main(directory: str) -> None:
       for f in glob(pattern)
     ]
     if files:
-      LOGGER.info(f"Rendering templated variables in '{folder}'")
+      LOGGER.info(f"Rendering templated variable files in '{folder}'")
       render_templates(environment, files, variables)
-      LOGGER.info(f"Loading templated variables in '{folder}'")
+      LOGGER.info(f"Loading templated variable files in '{folder}'")
       variables = merge_dicts(
         variables,
         load_variables([f[:-len(TEMPLATE_EXTENSION)] for f in files]),
       )
+    else:
+      LOGGER.info(f"No templated variable files found in '{folder}'.")
 
   LOGGER.info(f"Rendering templates")
   files = (f for f in glob(f'data/**/*{TEMPLATE_EXTENSION}') if not f.endswith(f'yml{TEMPLATE_EXTENSION}'))
@@ -69,12 +78,16 @@ def main(directory: str) -> None:
     render_templates(environment, files, variables)
 
 def create_template_environment(directory: str) -> Environment:
-  return Environment(
+  os.chdir(directory)
+  env = Environment(
     autoescape=False,
     extensions=[AnsibleCoreFiltersExtension],
-    loader=FileSystemLoader(directory),
+    loader=FileSystemLoader(
+      os.path.dirname('.')
+    ),
     trim_blocks=True,
   )
+  return env
 
 def load_variable_file(yaml_file: str) -> dict:
   LOGGER.debug(f"Loading variable file '{yaml_file}'")
@@ -106,15 +119,54 @@ def render_templates(environment: Environment, files: list[str], variables: dict
   if type(files) is not list:
     files = list(files)
   LOGGER.info(f"Rendering templates {files}")
-  for f in files:
-    LOGGER.debug(f"Rendering template '{f}'")
-    environment \
-      .get_template(f) \
-      .stream(variables) \
-      .dump(f[:-len(TEMPLATE_EXTENSION)])
+  with TemporaryDirectory() as tmpdir:
+    for f in files:
+      target = f[:-len(TEMPLATE_EXTENSION)]
+      target_tmp = os.path.join(tmpdir, target.replace('/', '\x1a'))
+      LOGGER.debug(f"Rendering template '{f}' to '{target_tmp}'")
+      environment \
+        .get_template(f) \
+        .stream(variables) \
+        .dump(target_tmp)
+      if CONFIG.diff:
+        with open(target, 'r') as render_old:
+          with open(target_tmp, 'r') as render_new:
+            diff = difflib.context_diff(
+              render_old.readlines(),
+              render_new.readlines(),
+              fromfile=target,
+            )
+            print(''.join(diff), end='')
+      if not CONFIG.dry_run:
+        shutil.copystat(target, target_tmp)
+        shutil.move(target_tmp, target)
 
+
+def parse_cli_config():
+  import argparse
+  parser = argparse.ArgumentParser(
+    "render_templates",
+    "Renders templated files (marked with '.j2' extension)",
+  )
+  parser.add_argument(
+    'directory',
+    help="Root directory to search for templates in.",
+    nargs='?',
+    default=os.getcwd(),
+  )
+  parser.add_argument(
+    '--diff',
+    help="Show differences between old and new rendered templates.",
+    action='store_true',
+  )
+  parser.add_argument(
+    '--dry-run',
+    help="Don't actually render the templates, only output diff to current state.",
+    action='store_true',
+  )
+  return parser.parse_args()
 
 if __name__ == '__main__':
-  directory = os.path.dirname(os.path.abspath(__file__))
-  main(directory)
+  CONFIG = parse_cli_config()
+  main(CONFIG.directory)
 
