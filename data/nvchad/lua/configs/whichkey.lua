6d4cc0b9ce1aac28bb78eee6a737d73ffc6fa721
Simon Lammer <lammer.simon@gmail.com>
2024-03-17 10:51:20 +0100

    nvim: Reconfigure whichkey

---
+++ data/nvchad/lua/configs/whichkey.lua
@@ -0,0 +1,37 @@
+return { -- https://github.com/NvChad/NvChad/issues/1246#issuecomment-1817582042
+  config = function(_, opts)
+    dofile(vim.g.base46_cache .. "whichkey")
+    require("which-key").setup(opts)
+    local present, wk = pcall(require, "which-key")
+    if not present then
+      return
+    end
+    wk.register({
+      ["<leader>"] = {
+        c = {name = "Code"},
+        f = {name = "Files"},
+        g = {name = "Git"},
+        l = {name = "LSP"},
+        r = {name = "Relative"},
+        t = {name = "Telescope"},
+        w = {name = "Whichkey"},
+      },
+      ["["] = {
+        name = "Previous..."
+      },
+      ["]"] = {
+        name = "Next..."
+      },
+      ["g"] = {
+        name = "Goto..."
+      },
+      ["z"] = {
+        name = "Fold"
+      },
+    })
+  end,
+  setup = function()
+    require("core.utils").load_mappings("whichkey")
+  end,
+}
+
