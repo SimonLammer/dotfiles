602ae02cabab85ccf1040fa7d86144c610034096
Simon Lammer <lammer.simon@gmail.com>
2024-03-20 15:54:04 +0100

    nvim: Add auto-session plugin

---
+++ data/nvchad/lua/plugins/30-auto-session.lua
@@ -0,0 +1,9 @@
+return {
+  "rmagatti/auto-session",
+  lazy=false,
+  opts = {
+    --auto_session_enabled = false,
+    auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+    --log_level = "error",
+  }
+}
