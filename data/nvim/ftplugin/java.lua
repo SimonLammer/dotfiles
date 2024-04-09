698bc8fbabf5368dfeeb322a79fdb3b7b5b25351
Simon Lammer <lammer.simon@gmail.com>
2024-04-09 17:17:48 +0200

    nvim: Fix jdtls binary path

--- data/nvim/ftplugin/java.lua
+++ data/nvim/ftplugin/java.lua
@@ -1,6 +1,6 @@
 -- https://www.youtube.com/watch?v=C7juSZsM2Fg
 local config = {
-  cmd = {vim.fn.expand '$XDG_DATA_HOME/$NVIM_APPNAME/mason/bin/jdtls'},
+  cmd = {vim.fn.expand '$XDG_DATA_HOME/' .. (os.getenv("NVIM_APPNAME") or "nvim") .. '/mason/bin/jdtls'},
   root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, {upward = true})[1]),
 }
 require("jdtls").start_or_attach(config)
