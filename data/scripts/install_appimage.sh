86c1fac497ee7494e6f9d88491d1d294af631dec
Simon Lammer <lammer.simon@gmail.com>
2025-04-29 16:12:02 +0200

    scripts/install_appimage.sh: Set file permissions

--- data/scripts/install_appimage.sh
+++ data/scripts/install_appimage.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/sh
+set -e
 
-if [ "$#" -lt "2"]; then
+if [ "$#" -lt "2" ]; then
   echo "Usage: $0 <program.AppImage> <program name>" >&2
   exit 1
 fi
 appimage=$1
 name=$2
 
+chmod 755
 dest=~/.local/bin
 mv "$appimage" "$dest"
 cd "$dest"
