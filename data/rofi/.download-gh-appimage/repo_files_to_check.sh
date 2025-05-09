f5cfb75b83f04b0c1b0185c6b4c43698ebbf608a
Simon Lammer <lammer.simon@gmail.com>
2025-05-10 01:18:41 +0200

    scripts/download-gh-appimage.sh: Write script to download AppImage files from GitHub

---
+++ data/rofi/.download-gh-appimage/repo_files_to_check.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+set -e
+
+#
+# This script lists all files that shall be checked by download-gh-appimage.sh
+# one file per line.
+#
+
+find .github/workflows -type f
+printf "\
+rofi-appimage.sh
+"
+
